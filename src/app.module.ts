b9b33effa81a97a38526fc812de030b60b146589
Tobi Balogun <tobi.balo@gmail.com>
2023-01-31 18:59:53 +0100

    new: set up articles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ArticlesModule } from './articles/articles.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ArticlesModule],
   controllers: [],
   providers: [],
 })
---
+++ src/articles/articles.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private readonly prisma: PrismaService) {}
+  create(createArticleDto: CreateArticleDto) {
+    return 'This action adds a new article';
+  }
+
+  async findAll() {
+    return await this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  async findAllDrafts() {
+    return await this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  async findOne(id: string) {
+    const article = await this.prisma.article.findUnique({ where: { id } });
+
+    if (!article) {
+      throw new NotFoundException('Article not found!');
+    }
+
+    return article;
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return `This action updates a #${id} article`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} article`;
+  }
+}
