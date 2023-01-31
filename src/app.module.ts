7a0bc6919fa936705c3c7d7a908040bcf6a97c01
Tobi Balogun <tobi.balo@gmail.com>
2023-01-31 06:31:15 +0100

    new: set up prisma module for interacting with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
