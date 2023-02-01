e095ac67c6e03dea65bf06ecf48dda8e61e8ab6c
Tobi Balogun <tobi.balo@gmail.com>
2023-02-01 07:48:50 +0100

    new: setup article entity for Swagger

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -7,35 +7,43 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('articles')
+@ApiTags('Articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   async create(@Body() createArticleDto: CreateArticleDto) {
     return await this.articlesService.create(createArticleDto);
   }
 
   @Get()
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   async findAll() {
     return await this.articlesService.findAll();
   }
 
   @Get('drafts')
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   async findAllDrafts() {
     return await this.articlesService.findAllDrafts();
   }
 
   @Get(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   async findOne(@Param('id') id: string) {
     return await this.articlesService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   async update(
     @Param('id') id: string,
     @Body() updateArticleDto: UpdateArticleDto,
@@ -44,6 +52,7 @@ export class ArticlesController {
   }
 
   @Delete(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   async remove(@Param('id') id: string) {
     return await this.articlesService.remove(id);
   }
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,19 @@
-export class Article {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Article } from '@prisma/client';
+
+export class ArticleEntity implements Article {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  title: string;
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+  @ApiProperty()
+  body: string;
+  @ApiProperty()
+  published: boolean;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+}
